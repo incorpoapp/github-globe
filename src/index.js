97079c21ffc444ed2b66ed81154db32b7b9e26d7
Ali Aliev <shamkir@gmail.com>
2022-01-14 20:14:28 +0100

    Code refactoring

--- src/index.js
+++ src/index.js
@@ -6,9 +6,6 @@ import {
   DirectionalLight,
   Color,
   Fog,
-  // AxesHelper,
-  // DirectionalLightHelper,
-  // CameraHelper,
   PointLight,
   SphereGeometry,
 } from "three";
@@ -17,185 +14,159 @@ import { createGlowMesh } from "three-glow-mesh";
 import countries from "./files/globe-data-min.json";
 import travelHistory from "./files/my-flights.json";
 import airportHistory from "./files/my-airports.json";
-var renderer, camera, scene, controls;
-let mouseX = 0;
-let mouseY = 0;
-let windowHalfX = 900 / 2;
-let windowHalfY = 900 / 2;
-var Globe;
 
-init();
-initGlobe();
-onWindowResize();
-animate();
 
-// SECTION Initializing core ThreeJS elements
-function init() {
-  // Initialize renderer
-  renderer = new WebGLRenderer({ antialias: true, alpha: true });
-  renderer.setClearColor( 0x000000, 0 );
-  renderer.setPixelRatio(window.devicePixelRatio);
-  renderer.setSize(900, 900);
-  // renderer.outputEncoding = THREE.sRGBEncoding;
-  const interval = setInterval(() => {
-    const element = document.getElementById('globe');
-    if (element !== null) {
-      element.appendChild(renderer.domElement);
-      clearInterval(interval);
-    }
-  }, 1000);
-  // Initialize scene, light
-  scene = new Scene();
-  scene.add(new AmbientLight(0xbbbbbb, 0.3));
-  scene.background = new Color(0x040d21);
+class Globe {
+  constructor(windowHalfX, windowHalfY, rendererDomElementId) {
+    this.windowHalfX = windowHalfX;
+    this.windowHalfY = windowHalfY;
+    this.mouseX = 0;
+    this.mouseY = 0;
+    this.renderer = new WebGLRenderer({ antialias: true, alpha: true });
+    this.camera = new PerspectiveCamera();
+    this.scene = new Scene();
+    this.controls = new OrbitControls(this.camera, this.renderer.domElement);
+    this.rendererDomElementId = rendererDomElementId;
+  }
 
-  // Initialize camera, light
-  camera = new PerspectiveCamera();
-  camera.aspect = 900 / 900;
-  camera.updateProjectionMatrix();
+  init() {
+    this.renderer.setClearColor( 0x000000, 0 );
+    this.renderer.setPixelRatio(window.devicePixelRatio);
+    this.renderer.setSize(700, 700);
+    this.renderer.setClearColor( 0xffffff, 0);
+    this.renderer.domElement.id = this.rendererDomElementId;
 
-  var dLight = new DirectionalLight(0xffffff, 0.8);
-  dLight.position.set(-800, 2000, 400);
-  camera.add(dLight);
+    this.scene.add(new AmbientLight(0xbbbbbb, 0.3));
 
-  var dLight1 = new DirectionalLight(0x7982f6, 1);
-  dLight1.position.set(-200, 500, 200);
-  camera.add(dLight1);
+    this.camera.aspect = 700 / 700;
+    this.camera.updateProjectionMatrix();
 
-  var dLight2 = new PointLight(0x8566cc, 0.5);
-  dLight2.position.set(-200, 500, 200);
-  camera.add(dLight2);
+    let dLight = new DirectionalLight(0xffffff, 0.8);
+    dLight.position.set(-800, 2000, 400);
+    this.camera.add(dLight);
 
-  camera.position.z = 400;
-  camera.position.x = 0;
-  camera.position.y = 0;
+    let dLight1 = new DirectionalLight(0x7982f6, 1);
+    dLight1.position.set(-200, 500, 200);
+    this.camera.add(dLight1);
 
-  scene.add(camera);
+    let dLight2 = new PointLight(0x8566cc, 0.5);
+    dLight2.position.set(-200, 500, 200);
+    this.camera.add(dLight2);
 
-  // Additional effects
-  scene.fog = new Fog(0x535ef3, 400, 2000);
+    this.camera.position.z = 290;
+    this.camera.position.x = 0;
+    this.camera.position.y = 0;
 
-  // Helpers
-  // const axesHelper = new AxesHelper(800);
-  // scene.add(axesHelper);
-  // var helper = new DirectionalLightHelper(dLight);
-  // scene.add(helper);
-  // var helperCamera = new CameraHelper(dLight.shadow.camera);
-  // scene.add(helperCamera);
+    this.scene.add(this.camera);
 
-  // Initialize controls
-  controls = new OrbitControls(camera, renderer.domElement);
-  controls.enableDamping = true;
-  controls.dynamicDampingFactor = 0.01;
-  controls.enablePan = false;
-  controls.minDistance = 200;
-  controls.maxDistance = 500;
-  controls.rotateSpeed = 0.8;
-  controls.zoomSpeed = 1;
-  controls.autoRotate = false;
-  controls.enableZoom = false;
+    this.scene.fog = new Fog(0x535ef3, 400, 2000);
 
-  controls.minPolarAngle = Math.PI / 3.5;
-  controls.maxPolarAngle = Math.PI - Math.PI / 3;
+    this.controls.enableDamping = true;
+    this.controls.dynamicDampingFactor = 0.01;
+    this.controls.enablePan = false;
+    this.controls.minDistance = 200;
+    this.controls.maxDistance = 500;
+    this.controls.rotateSpeed = 0.8;
+    this.controls.zoomSpeed = 1;
+    this.controls.autoRotate = false;
+    this.controls.enableZoom = false;
 
-  window.addEventListener("resize", onWindowResize, false);
-  document.addEventListener("mousemove", onMouseMove);
+    this.controls.minPolarAngle = Math.PI / 3.5;
+    this.controls.maxPolarAngle = Math.PI - Math.PI / 3;
+
+    return this.renderer.domElement;
+  }
+
+  animate() {
+    this.controls.update();
+    this.renderer.render(this.scene, this.camera);
+    requestAnimationFrame(this.animate.bind(this));
+  }
+
+  initGlobe() {
+    let globe = new ThreeGlobe({
+      waitForGlobeReady: true,
+      animateIn: true,
+    })
+      .hexPolygonsData(countries.features)
+      .hexPolygonResolution(3)
+      .hexPolygonMargin(0.7)
+      .showAtmosphere(true)
+      .atmosphereColor("#3a228a")
+      .atmosphereAltitude(0.25)
+      .hexPolygonColor((e) => {
+        if (
+          ["KGZ", "KOR", "THA", "RUS", "UZB", "IDN", "KAZ", "MYS"].includes(
+            e.properties.ISO_A3
+          )
+        ) {
+          return "rgba(255,255,255, 1)";
+        } else return "rgba(255,255,255, 0.7)";
+      });
+
+    // NOTE Arc animations are followed after the globe enters the scene
+    setTimeout(() => {
+      globe.arcsData(travelHistory.flights)
+        .arcColor((e) => {
+          return e.status ? "#9cff00" : "#FF4000";
+        })
+        .arcAltitude((e) => {
+          return e.arcAlt;
+        })
+        .arcStroke((e) => {
+          return e.status ? 0.5 : 0.3;
+        })
+        .arcDashLength(0.9)
+        .arcDashGap(4)
+        .arcDashAnimateTime(1000)
+        .arcsTransitionDuration(1000)
+        .arcDashInitialGap((e) => e.order * 1)
+        .labelsData(airportHistory.airports)
+        .labelColor(() => "#ffcb21")
+        .labelDotOrientation((e) => {
+          return e.text === "ALA" ? "top" : "right";
+        })
+        .labelDotRadius(0.3)
+        .labelSize((e) => e.size)
+        .labelText("city")
+        .labelResolution(6)
+        .labelAltitude(0.01)
+        .pointsData(airportHistory.airports)
+        .pointColor(() => "#ffffff")
+        .pointsMerge(true)
+        .pointAltitude(0.07)
+        .pointRadius(0.05);
+    }, 1000);
+
+    globe.rotateY(-Math.PI * (5 / 9));
+    globe.rotateZ(-Math.PI / 6);
+
+    const globeMaterial = globe.globeMaterial();
+    globeMaterial.color = new Color(0x3a228a);
+    globeMaterial.emissive = new Color(0x220038);
+    globeMaterial.emissiveIntensity = 0.1;
+    globeMaterial.shininess = 0.7;
+
+    // NOTE Cool stuff
+    globeMaterial.wireframe = true;
+
+    this.scene.add(globe);
+  }
+
+  setAspectSize(windowHalfX, windowHalfY) {
+    this.camera.aspect = windowHalfX / windowHalfY;
+    this.camera.updateProjectionMatrix();
+    this.windowHalfX = windowHalfX / 1.5;
+    this.windowHalfY = windowHalfY / 1.5;
+    this.renderer.setSize(windowHalfX, windowHalfY);
+  }
 }
 
-// SECTION Globe
-function initGlobe() {
-  // Initialize the Globe
-  Globe = new ThreeGlobe({
-    waitForGlobeReady: true,
-    animateIn: true,
-  })
-    .hexPolygonsData(countries.features)
-    .hexPolygonResolution(3)
-    .hexPolygonMargin(0.7)
-    .showAtmosphere(true)
-    .atmosphereColor("#3a228a")
-    .atmosphereAltitude(0.25)
-    .hexPolygonColor((e) => {
-      if (
-        ["KGZ", "KOR", "THA", "RUS", "UZB", "IDN", "KAZ", "MYS"].includes(
-          e.properties.ISO_A3
-        )
-      ) {
-        return "rgba(255,255,255, 1)";
-      } else return "rgba(255,255,255, 0.7)";
-    });
+window.Globe = Globe;
 
-  // NOTE Arc animations are followed after the globe enters the scene
-  setTimeout(() => {
-    Globe.arcsData(travelHistory.flights)
-      .arcColor((e) => {
-        return e.status ? "#9cff00" : "#FF4000";
-      })
-      .arcAltitude((e) => {
-        return e.arcAlt;
-      })
-      .arcStroke((e) => {
-        return e.status ? 0.5 : 0.3;
-      })
-      .arcDashLength(0.9)
-      .arcDashGap(4)
-      .arcDashAnimateTime(1000)
-      .arcsTransitionDuration(1000)
-      .arcDashInitialGap((e) => e.order * 1)
-      .labelsData(airportHistory.airports)
-      .labelColor(() => "#ffcb21")
-      .labelDotOrientation((e) => {
-        return e.text === "ALA" ? "top" : "right";
-      })
-      .labelDotRadius(0.3)
-      .labelSize((e) => e.size)
-      .labelText("city")
-      .labelResolution(6)
-      .labelAltitude(0.01)
-      .pointsData(airportHistory.airports)
-      .pointColor(() => "#ffffff")
-      .pointsMerge(true)
-      .pointAltitude(0.07)
-      .pointRadius(0.05);
-  }, 1000);
+// let g = new Globe(900, 900, "globe");
+// let domElement = g.init();
+// g.initGlobe();
+// g.animate()
 
-  Globe.rotateY(-Math.PI * (5 / 9));
-  Globe.rotateZ(-Math.PI / 6);
-  const globeMaterial = Globe.globeMaterial();
-  globeMaterial.color = new Color(0x3a228a);
-  globeMaterial.emissive = new Color(0x220038);
-  globeMaterial.emissiveIntensity = 0.1;
-  globeMaterial.shininess = 0.7;
-
-  // NOTE Cool stuff
-  globeMaterial.wireframe = true;
-
-  scene.add(Globe);
-}
-
-function onMouseMove(event) {
-  mouseX = event.clientX - windowHalfX;
-  mouseY = event.clientY - windowHalfY;
-  // console.log("x: " + mouseX + " y: " + mouseY);
-}
-
-function onWindowResize() {
-  camera.aspect = 900 / 900;
-  camera.updateProjectionMatrix();
-  windowHalfX = 900 / 1.5;
-  windowHalfY = 900 / 1.5;
-  renderer.setSize(900, 900);
-  renderer.domElement.id = 'globe-canvas';
-}
-
-function animate() {
-  // camera.position.x +=
-  //   Math.abs(mouseX) <= windowHalfX / 2
-  //     ? (mouseX / 2 - camera.position.x) * 0.005
-  //     : 0;
-  // camera.position.y += (-mouseY / 2 - camera.position.y) * 0.005;
-  // camera.lookAt(scene.position);
-  controls.update();
-  renderer.render(scene, camera);
-  requestAnimationFrame(animate);
-}
+// document.body.appendChild(domElement);
